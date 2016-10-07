0725a43568b4505bff00045c56e07d79475afb5c
UserPL022Pc08 <UserPL022Pc08@SLFRPL022PC08.ensino.atec.local>
2016-10-07 15:37:12 +0100

    Exercicio 0710

---
+++ src/exercicios071016/Treino.java
@@ -0,0 +1,20 @@
+package exercicios071016;
+
+import java.util.Scanner;
+
+public class Treino {
+
+	public Treino() {
+		exercicio1();
+	}
+	
+	private void exercicio1() {
+		int num1 =  (new Scanner(System.in)).nextInt();
+		int num2 =  (new Scanner(System.in)).nextInt();
+		
+		if (num1 > num2) {
+			System.out.println("o numero 1 é o maior");
+		}
+		else System.out.println("o numero 2 é o maior");
+	}
+}
